9e7dee2e2869ec60189125b3c7cc72a8cb18da76
Wellington <wellington.dev89@hotmail.com>
2025-05-20 22:10:30 -0300

    Game repository, service , controller

---
+++ src/main/java/com/devsuperior/dslist/services/GameService.java
@@ -0,0 +1,22 @@
+package com.devsuperior.dslist.services;
+
+import com.devsuperior.dslist.dto.GameMinDTO;
+import com.devsuperior.dslist.entities.Game;
+import com.devsuperior.dslist.repositories.GameRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class GameService {
+
+    @Autowired //injeta outro repositorio
+    private GameRepository gameRepository;
+
+    public List<GameMinDTO> findAll() {
+        List<Game> result = gameRepository.findAll();
+        return result.stream().map(x -> new GameMinDTO(x)).toList();
+    }
+}
